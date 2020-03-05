121a2ae7a39196bbc317b6b02c25065f704808d4
Tiago Baptista <61202028+tiagob99@users.noreply.github.com>
2020-03-05 15:53:48 +0000

    t

--- usar/nova_teste/src/components/Mikros.jsx
+++ usar/nova_teste/src/components/Mikros.jsx
@@ -12,7 +12,8 @@ const Mikros = () => {
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
         <script src="http://code.jquery.com/ui/1.8.20/jquery-ui.min.js" type="text/javascript"></script>
         <script src="jscolor.js"></script>
-        <script src="Mikro.js"></script>
+        <script src="mikrosjs.js"></script>
+        <body onload='timerTick()'>
         <div class="big_div">
         <div id="toggle-btn" onclick="togglemenu()">
           <span></span>
@@ -30,7 +31,7 @@ const Mikros = () => {
                       <line id='hour_pointer' x1='50' y1='50' x2='50' y2='27' />
                       <line id='minute_pointer' x1='50' y1='50' x2='50' y2='17' />
                       <line id='second_pointer' x1='50' y1='50' x2='50' y2='13' />
-                      {/* <circle id="center_dot" style="stroke:white; stroke-width:3px; "cx="50" cy="50" r="1"/> */}
+                      {/* <circle id="center_dot" style="{stroke:white; stroke-width:3px;} "cx="50" cy="50" r="1"/> */}
                     </g>
                   </svg>
                 </div>
@@ -39,40 +40,40 @@ const Mikros = () => {
             <div id="remaining_01">
               <li class="elementos_menu_direito" onclick="togglemenu2()">Colors</li>
               <div id="second_menu_hidden">
-                <p>Background Colors <input class="jscolor {onFineChange:'background_color(this)'}" value="000"> </input> </p>
-                <p>Digital <input class="jscolor {onFineChange:'digital_color(this)'}" value="fff"></input> </p>
-                <p>Analog Hours <input class="jscolor {onFineChange:'analog_hours(this)'}" value="fff"></input> </p>
-                <p>Analog Minuts <input class="jscolor {onFineChange:'analog_minutes(this)'}" value="fff"></input> </p>
-                <p>Analog Seconds <input class="jscolor {onFineChange:'analog_seconds(this)'}" value="FF0000"></input> </p>
-                <p>Center Dot <input class="jscolor {onFineChange:'center_circule(this)'}" value="fff"></input> </p>
+                <p>Background Colors <input class="jscolor {onFineChange:'background_color(this)'}" value="000"/> </p>
+                <p>Digital <input class="jscolor {onFineChange:'digital_color(this)'}" value="fff"/></p>
+                <p>Analog Hours <input class="jscolor {onFineChange:'analog_hours(this)'}" value="fff"/></p>
+                <p>Analog Minuts <input class="jscolor {onFineChange:'analog_minutes(this)'}" value="fff"/></p>
+                <p>Analog Seconds <input class="jscolor {onFineChange:'analog_seconds(this)'}" value="FF0000"/></p>
+                <p>Center Dot <input class="jscolor {onFineChange:'center_circule(this)'}" value="fff"/></p>
               </div>
               <div id="remaining_02">
                 <li class="elementos_menu_direito"  onclick="togglemenu3()">Line</li>
                 <div id="third_menu_hidden">
-                  <p>Circle Bar 1</p> <br> <input class="jscolor {onFineChange:'bar_color1(this)'}" name="b1_color" value="FF0000" ></input></br>
-                  <p>Circle Bar 2</p> <br> <input class="jscolor {onFineChange:'bar_color2(this)'}" name="b2_color" value="00acc1" ></input></br>
-                  <p>Circle Bar 1 Size </p><br> <input id="bar_size1"  value="100" max="89" onchange="bar_thickness_1()" type="range" name="b_size" ></input></br>
-                  <p>Circle Bar 2 Size </p> <br> <input id="bar_size2" value="100" max="89" onchange="bar_thickness_2()" type="range" name="b_size" ></input></br>
-                  <p>Circle Bar Radius </p> <br> <input id="bar_radius" min="59" onchange="bars_radius()" type="range" name="b_radius" value="100"></input></br>
-                  <p>Circle Bar Start </p><br> </br>
+                  <p>Circle Bar 1</p> <br/> <input class="jscolor {onFineChange:'bar_color1(this)'}" name="b1_color" value="FF0000" />
+                  <p>Circle Bar 2</p> <br/> <input class="jscolor {onFineChange:'bar_color2(this)'}" name="b2_color" value="00acc1" />
+                  <p>Circle Bar 1 Size </p> <br/> <input id="bar_size1"  value="100" max="89" onchange="bar_thickness_1()" type="range" name="b_size" />
+                  <p>Circle Bar 2 Size </p> <br/> <input id="bar_size2" value="100" max="89" onchange="bar_thickness_2()" type="range" name="b_size" />
+                  <p>Circle Bar Radius </p> <br/> <input id="bar_radius" min="59" onchange="bars_radius()" type="range" name="b_radius" value="100"/>
+                  <p>Circle Bar Start </p><br/> 
                   <select name="bar_star" id="bar_type" onchange="bar_type()" >
                     <option value="full">Full</option>
                     <option value="tophalf">Top Half</option>
                     <option value="bottomhalf">Bottom Half</option>
                   </select>
-                  <br></br>
-                  <p>Circle Bar Rotation </p><br> </br>
+                  <br/>
+                  <p>Circle Bar Rotation </p><br/>
                   <select name="bar_rotation" id="bar_star" onchange="update_rotation()" >
                     <option value="clockwise">Clockwise</option>
                     <option value="counterclockwise">Counterclockwise</option>
                   </select>
-                  <br></br>
+                  <br/>
                 </div>
                 <div id="remaining_03">
                   <li class="elementos_menu_direito" onclick="togglemenu4()">Attributes</li>
                   <div id="fourth_menu_hidden">
                     <div class="text">
-                      <p>Message</p><br></br>
+                      <p>Message</p><br/>
                       <textarea name="field5" class="tex_box" placeholder="Insert your message" rows="15" cols="45"></textarea>
                     </div>
                   </div>
@@ -85,11 +86,11 @@ const Mikros = () => {
           <div class="center_circule">
             <p id="rect" class="center_circule" ></p>
             <div class="circule">
-              <svg class="circle-chart" viewbox="0 0 33.83098862 33.83098862" width="90%" height="90%" style="position:absolute; left:50%; top: 50%; margin-left: -45%; margin-top:-45%;">
+              {/* <svg class="circle-chart" viewbox="0 0 33.83098862 33.83098862" width="90%" height="90%" style="position:absolute; left:50%; top: 50%; margin-left: -45%; margin-top:-45%;">
                 <circle class="circle-chart__circle" id="bar1" stroke="#FF0000" stroke-width="2" stroke-dasharray="65,100"  fill="none"  cx="16.91549431" cy="16.91549431" r="16" />
                 <circle class="circle-chart__circle" id="bar2" stroke="#00acc1" stroke-width="2" stroke-dasharray="30,100"  fill="none"  cx="16.91549431" cy="16.91549431" r="14" />
   
-              </svg>
+              </svg> */}
             </div>
             <div id="dropdiv" >
               <div class="child elementos "id="node1" >
@@ -105,6 +106,7 @@ const Mikros = () => {
           </div>
         </div>
         </div>
+        </body>
         </div> 
       
     ); 
---
+++ usar/nova_teste/src/components/mikrosjs.js
@@ -0,0 +1,240 @@
+/*Menu*/
+function togglemenu(){
+ document.getElementById('sidebar').classList.toggle('active');
+ document.getElementById('toggle-btn').classList.toggle('active');
+}
+function togglemenu1(){
+ document.getElementById('first_menu_hidden').classList.toggle('active');
+ document.getElementById('remaining_01').classList.toggle('active');
+}
+function togglemenu2(){
+ document.getElementById('second_menu_hidden').classList.toggle('active');
+ document.getElementById('remaining_02').classList.toggle('active');
+}
+function togglemenu3(){
+ document.getElementById('third_menu_hidden').classList.toggle('active');
+ document.getElementById('remaining_03').classList.toggle('active');
+}
+function togglemenu4(){
+ document.getElementById('fourth_menu_hidden').classList.toggle('active');
+}
+/*Menu*/
+
+/*drag and drop*/
+/*drag and drop*/
+var drop = "node2"
+$(function() {
+    initSwap();
+    function initSwap() {
+    initDroppable($("#dropdiv div,#dragdiv div"));
+    initDraggable($("#dragdiv div,#dropdiv div"));
+    }
+    function initDraggable($elements) {
+    $elements.draggable({
+        appendTo: "body",
+        helper: "clone",
+        revert: "invalid"
+    });
+    }
+    function initDroppable($elements) {
+    $elements.droppable({
+        activeClass: "ui-state-default",
+        hoverClass: "ui-drop-hover",
+        accept: ":not(.ui-sortable-helper)",
+        over: function(event, ui) {
+            var $this = $(this);
+        },
+        drop: function(event, ui) {
+          var $this = $(this);
+          var linew1 = $(this).after(ui.draggable.clone());
+          var linew2 = $(ui.draggable).after($(this).clone());
+
+          var moved = ui.draggable.attr("id");// Obtem o id do elemento sendo arrastado
+
+          if (moved==drop) {//Caso seja arrastado da direita para a esquerda
+            drop = $(this).attr("id");
+          }
+          else{ //Caso seja arrastado da esquerda para a direita
+            drop = moved;
+          }
+            $(ui.draggable).remove();
+            $(this).remove();
+            initSwap();
+        }
+    });
+  }
+});
+
+
+function readropzone() {
+  if (drop == "node1") {
+    console.log("Digital");
+  }
+  else {
+    console.log("Analogico");
+  }
+}
+/*drag and drop*/
+
+
+
+/*Relogio Digital*/
+function clock(){
+  const fullDate = new Date();
+  var hours = fullDate.getHours();
+  var minutes = fullDate.getMinutes();
+  if (hours<10){
+    hours = "0" + hours
+  }
+  if (minutes < 10){
+    minutes = "0" + minutes
+  }
+  document.getElementById('hour').innerHTML = hours;
+  document.getElementById('minute').innerHTML = ": " + minutes;
+}
+setInterval(clock,100);
+/*Relogio Digital*/
+
+
+
+/*Relogio analogico*/
+function timerTick() {
+  with (new Date()) {
+    var h = 30 * ((getHours() % 12) + getMinutes() / 60);
+    var m = 6 * getMinutes();
+    var s = 6 * getSeconds();
+    document.getElementById('hour_pointer').setAttribute('transform', 'rotate(' + h + ', 50, 50)');
+    document.getElementById('minute_pointer').setAttribute('transform', 'rotate(' + m + ', 50, 50)');
+    document.getElementById('second_pointer').setAttribute('transform', 'rotate(' + s + ', 50, 50)');
+    setTimeout(timerTick, 100);
+  }
+}
+/*Relogio analogico*/
+
+
+
+/*Cores*/
+function background_color(jscolor) {
+    document.getElementById('rect').style.backgroundColor = '#' + jscolor
+}
+function digital_color(jscolor) {
+    document.getElementById('node1').style.color = '#' + jscolor
+}
+function analog_hours(jscolor) {
+    document.getElementById('hour_pointer').style.stroke = '#' + jscolor
+}
+function analog_minutes(jscolor) {
+    document.getElementById('minute_pointer').style.stroke = '#' + jscolor
+}
+function analog_seconds(jscolor) {
+    document.getElementById('second_pointer').style.stroke = '#' + jscolor
+}
+function center_circule(jscolor) {
+    document.getElementById('center_dot').style.stroke = '#' + jscolor
+}
+function bar_color1(jscolor) {
+    document.getElementById('bar1').style.stroke = '#' + jscolor
+}
+function bar_color2(jscolor) {
+    document.getElementById('bar2').style.stroke = '#' + jscolor
+}
+/*Cores*/
+
+
+
+/*personalização*/
+var tamanho_barra1 = 65;
+var tamanho_barra2 = 30;
+var raio1 = 16;
+var raio2 = 14;
+var right = true;
+var degrees= -90;
+var position = "full"
+
+
+function bars_radius() {
+    var novo = document.getElementById('bar_radius').value;
+    novo = (novo/100)*15+1;
+    raio1 = novo;
+    raio2 = raio1 - 2;
+    document.getElementById('bar1').style.r =  raio1;
+    document.getElementById('bar1').style.strokeDasharray = (2*3.1415*raio1)*(tamanho_barra1/100)+",100";
+    document.getElementById('bar2').style.r =  raio2;
+    document.getElementById('bar2').style.strokeDasharray = (2*3.1415*raio2)*(tamanho_barra2/100)+",100";
+}
+
+
+function bar_thickness_1() {
+    var novo = document.getElementById('bar_size1').value;
+    novo = (novo/100)*2+0.2;
+    document.getElementById('bar1').style.strokeWidth =  novo;
+}
+
+
+function bar_thickness_2() {
+    var novo = document.getElementById('bar_size2').value;
+    novo = (novo/100)*2+0.2;
+    document.getElementById('bar2').style.strokeWidth =  novo;
+}
+
+
+function bar_type(){
+  var novo = document.getElementById('bar_type').value;
+  if (novo == "full"){
+    document.getElementById('bar2').style.strokeDasharray  = "30,100"
+    document.getElementById('bar2').style.transform = "rotate(" + 270 + "deg)"
+    document.getElementById('bar1').style.strokeDasharray  = "65,100"
+    document.getElementById('bar1').style.transform = "rotate(" + 270 + "deg)"
+    position = "full"
+    degrees = -90;
+    tamanho_barra1 = 65;
+    tamanho_barra2 = 30;
+    bars_radius()
+  }
+  else if (novo == "tophalf") {
+    document.getElementById('bar2').style.transform = "rotate(" + 180 + "deg)"
+    document.getElementById('bar2').style.strokeDasharray  = "30,100"
+    document.getElementById('bar1').style.transform = "rotate(" + 180 + "deg)"
+    document.getElementById('bar1').style.strokeDasharray  = "50,100"
+    position = "tophalf"
+    degrees = -180;
+    tamanho_barra1 = 50;
+    tamanho_barra2 = 30;
+    bars_radius()
+  }
+  else {
+    document.getElementById('bar2').style.transform = "rotate(" + 0 + "deg)"
+    document.getElementById('bar2').style.strokeDasharray  = "30,100"
+    document.getElementById('bar1').style.transform = "rotate(" + 0 + "deg)"
+    document.getElementById('bar1').style.strokeDasharray  = "50,100"
+    position = "bottomhalf"
+    degrees = 0;
+    tamanho_barra1 = 50;
+    tamanho_barra2 = 30;
+    bars_radius()
+  }
+}
+
+
+function update_rotation(){
+  if (right){
+    if(position == "full"){
+      var novo = degrees -180
+      document.getElementById('bar2').style.transform = "rotate(" + novo + "deg) scaleX(-1)"
+      document.getElementById('bar1').style.transform = "rotate(" + novo + "deg) scaleX(-1)"
+      right = false;
+    }
+    else {
+      var novo = degrees
+      document.getElementById('bar2').style.transform = "rotate(" + novo + "deg) scaleX(-1)"
+      document.getElementById('bar1').style.transform = "rotate(" + novo + "deg) scaleX(-1)"
+      right = false;
+    }
+  }
+  else {
+    document.getElementById('bar2').style.transform = "rotate(" + degrees  + "deg) "
+    document.getElementById('bar1').style.transform = "rotate(" + degrees  + "deg) "
+    right = true;
+  }
+}
+/*personalização*/
