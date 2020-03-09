6cd906a3cfd71514f9741c50d9d276cc282fe48f
LuisGoncalves0 <luischgoncalves@hotmail.com>
2020-03-09 18:59:43 +0000

    dd

--- usar/nova_teste/src/components/Gardens.jsx
+++ usar/nova_teste/src/components/Gardens.jsx
@@ -11,77 +11,63 @@ import './jscolor';
 const Gardens = () => {
     return (
 
-      <div> 
-      {/* <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-      <script src="http://code.jquery.com/ui/1.8.20/jquery-ui.min.js" type="text/javascript"></script>
-      <script src="jscolor.js"></script>
-      <script src="Garden.js"></script> */}
-      <meta charset="utf-8" />
-      <title>Garden</title>
-      <link rel="stylesheet" type= "text/css" href="Garden.css"/>
-    
-    {/* <body  onload='timerTick()'> */}
-      <div class="big_div">
-        <div id="toggle-btn" onclick="togglemenu()">
-          <span></span>
-          <span></span>
-          {/* <span></span> */}
-        </div>
-        <div id="sidebar" >
-          <ul>
-            <li class="elementos_menu_direito" onclick="togglemenu1()" >Watch Types</li>
-            <div id="first_menu_hidden" >
-              <div id="dragdiv">
-                  <div class="child elementos "id="node1" >
-                    <svg height="70" width="200">
-                      <text x="0" y="15" ></text>
-                    </svg>
-                    <span id="hour"></span>
-                    <span id="minute"></span>
-                    <span id="second"></span>
-                  </div>
-  
-             </div>
+      <div>
+  <meta charSet="utf-8" />
+  <title>Garden</title>
+  <link rel="stylesheet" type="text/css" href="Garden.css" />
+  <div className="big_div">
+    <div id="toggle-btn" onclick="togglemenu()">
+      <span />
+      <span />
+      <span />
+    </div>
+    <div id="sidebar">
+      <ul>
+        <li className="elementos_menu_direito" onclick="togglemenu1()">Watch Types</li>
+        <div id="first_menu_hidden">
+          <div id="dragdiv">
+            <div className="child elementos " id="node1">
+              <svg height={70} width={200}>
+                <text x={0} y={15} />
+              </svg>
+              <span id="hour" />
+              <span id="minute" />
+              <span id="second" />
             </div>
-            <div id="remaining_01">
-              <li class="elementos_menu_direito" onclick="togglemenu2()">Colors</li>
-              <div id="second_menu_hidden">
-                <p>Background Colors <input class="jscolor {onFineChange:'background_color(this)'}" value="000"/> </p>
-                <p>Digital <input class="jscolor {onFineChange:'digital_color(this)'}" value="fff"/> </p>
-                <p>Analog Hours <input class="jscolor {onFineChange:'analog_hours(this)'}" value="fff"/> </p>
-                <p>Analog Minuts <input class="jscolor {onFineChange:'analog_minutes(this)'}" value="fff"/> </p>
-                <p>Analog Seconds <input class="jscolor {onFineChange:'analog_seconds(this)'}" value="ff0000"/> </p>
-                <p>Center Dot <input class="jscolor {onFineChange:'center_circule(this)'}" value="fff"/> </p>
-              </div>
-  
-            </div>
-          </ul>
-        </div>
-        <div class="watch">
-  
-          <div class="center_circule">
-            <p id="rect" class="center_circule" ></p>
-  
-            <div id="dropdiv" >
-              <div class="child elementos" id="node2">
-                <svg id='clock' viewBox='0 0 100 100' width='200' height='200'>
-                  <g id='pointers'>
-                    <line id='hour_pointer' x1='50' y1='50' x2='50' y2='27' />
-                    <line id='minute_pointer' x1='50' y1='50' x2='50' y2='17' />
-                    <line id='second_pointer' x1='50' y1='50' x2='50' y2='13' />
-                    <circle id="center_dot" style={{stroke: 'white', strokewidth:'3px'}} cx={50} cy={50} r={1}/> 
-                    {/* aqui alterei  mas onde viste esta alteração?                 */}
-                  </g>
-                </svg>
-              </div>
-            </div>
-  
           </div>
         </div>
-  
-      </div>
-    {/* </body> */}
+        <div id="remaining_01">
+          <li className="elementos_menu_direito" onclick="togglemenu2()">Colors</li>
+          <div id="second_menu_hidden">
+            <p>Background Colors <input className="jscolor {onFineChange:'background_color(this)'}" defaultValue={000} /> </p>
+            <p>Digital <input className="jscolor {onFineChange:'digital_color(this)'}" defaultValue="fff" /> </p>
+            <p>Analog Hours <input className="jscolor {onFineChange:'analog_hours(this)'}" defaultValue="fff" /> </p>
+            <p>Analog Minuts <input className="jscolor {onFineChange:'analog_minutes(this)'}" defaultValue="fff" /> </p>
+            <p>Analog Seconds <input className="jscolor {onFineChange:'analog_seconds(this)'}" defaultValue="ff0000" /> </p>
+            <p>Center Dot <input className="jscolor {onFineChange:'center_circule(this)'}" defaultValue="fff" /> </p>
+          </div>
+        </div>
+      </ul>
     </div>
+    <div className="watch">
+      <div className="center_circule">
+        <p id="rect" className="center_circule" />
+        <div id="dropdiv">
+          <div className="child elementos" id="node2">
+            <svg id="clock" viewBox="0 0 100 100" width={200} height={200}>
+              <g id="pointers">
+                <line id="hour_pointer" x1={50} y1={50} x2={50} y2={27} />
+                <line id="minute_pointer" x1={50} y1={50} x2={50} y2={17} />
+                <line id="second_pointer" x1={50} y1={50} x2={50} y2={13} />
+                <circle id="center_dot" style={{stroke: 'white', strokeWidth: '3px'}} cx={50} cy={50} r={1} />
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
   ); 
 }
