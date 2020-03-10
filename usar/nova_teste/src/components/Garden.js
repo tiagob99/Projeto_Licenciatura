edf0e0f85d25931ca095d4dd495ebb9e40349a2a
LuisGoncalves0 <luischgoncalves@hotmail.com>
2020-03-10 14:35:46 +0000

    hggh

--- usar/nova_teste/src/components/Garden.js
+++ usar/nova_teste/src/components/Garden.js
@@ -80,8 +80,9 @@ $(function readropzone() {
 
 
 /*Relogio Digital*/
-$(function clock(){
-  const fullDate = new Date();
+$( function clock(){
+setInterval( () => {
+const fullDate = new Date();
   var hours = fullDate.getHours();
   var minutes = fullDate.getMinutes();
   if (hours<10){
@@ -90,15 +91,17 @@ $(function clock(){
   if (minutes < 10){
     minutes = "0" + minutes
   }
- document.getElementById('hour').innerHTML = hours;
- document.getElementById('minute').innerHTML = ": " + minutes;
-  setInterval(clock,100);
+
+  document.getElementById('hour').innerHTML = hours;
+  document.getElementById('minute').innerHTML = ": " + minutes;
+  }, 100);
+
 })
 /*Relogio Digital*/
 
 
 /*Relogio analogico -- foi preciso colocar o new date numa variável certo??*/ 
-$(function timerTick() {
+export function timerTick() {
   const aqui =new Date();
     var h = 30 * ((aqui.getHours() % 12) + aqui.getMinutes() / 60);
     var m = 6 * aqui.getMinutes();
@@ -109,7 +112,6 @@ $(function timerTick() {
     setTimeout(timerTick, 100);
     setInterval(timerTick,100);
   }
-)
 /*Relogio analogico*/
 
 
--- usar/nova_teste/src/index.js
+++ usar/nova_teste/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+// import React from 'react';
+import React, {lazy} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import './main.scss';
 import makeYourOwn from './components/MakeYourOwn';
 import predefine from './components/Predefine';
-import Gardens from './components/Gardens';    
+// import Gardens from './components/Gardens';    
 import Mikros from './components/Mikros';
 import Login from './components/Login/Login';
 import Footer from './components/header-footer/Footer';
 import Header from './components/header-footer/Header';
+
+
+const home = lazy(() => import ('./components/Gardens')); 
 
 const Routes = () => (
     <BrowserRouter>
@@ -19,7 +23,7 @@ const Routes = () => (
             <Route exact path="/MakeYourOwn" component={makeYourOwn}/>
             <Route exact path="/Predefine" component={predefine}/>
             <Route exact path="/Login" component={Login}/>
-            <Route exact path="/Gardens" component={Gardens}/>
+            <Route exact path="/Gardens" component={home}/>
             <Route exact path="/Mikros" component={Mikros}/>
             <Footer/>            
         </div>
