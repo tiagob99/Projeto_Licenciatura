7798b53bb6d82e33cc402d1f56526ca3f0247afd
LuisGoncalves0 <luischgoncalves@hotmail.com>
2020-04-06 21:06:18 +0100

    ddf

--- usar/nova_teste/src/components/Mikros.jsx
+++ usar/nova_teste/src/components/Mikros.jsx
@@ -3,6 +3,7 @@ import { Flip } from 'react-reveal';
 import 'jquery-ui-dist/jquery-ui';
 import * as micro from './mikrosjs';
 import * as color from'./jscolor';
+import './mikrosjs';
 
 const Mikros = () => {
        
@@ -71,8 +72,39 @@ const Mikros = () => {
                   <li className="elementos_menu_direito" onClick={micro.togglemenu4}>Attributes</li>
                   <div id="fourth_menu_hidden">
                     <div className="text">
-                      <p>Message</p><br />
-                      <textarea name="field5" className="tex_box" placeholder="Insert your message" rows={15} cols={45} defaultValue={""} />
+                      {/* <textarea name="field5" className="tex_box" placeholder="Insert your message" rows={15} cols={45} defaultValue={""} /> */}
+                          <br /><br /><p>Text</p><br />
+                        <select id="first_variable">
+                          <option value="none">None</option>
+                          <option value="yesterday">Yesterday</option>
+                          <option value="today">Today</option>
+                          <option value="goal">Goal</option>
+                          <option value="other_user">Other User</option>
+                        </select>
+                        <textarea style={{backgroundColor: 'rgb(225,225,225,0)', zIndex: 8, resize: 'none', textAlign: 'justify', color: 'white'}} id="second_variable" name="field5" className="tex_box" placeholder="Insert your text" rows={1} cols={18} defaultValue={""} />
+                        <select id="third_variable">
+                          <option value="none">None</option>
+                          <option value="yesterday">Yesterday</option>
+                          <option value="today">Today</option>
+                          <option value="goal">Goal</option>
+                          <option value="other_user">Other User</option>
+                        </select>
+                        <textarea style={{backgroundColor: 'rgb(225,225,225,0)', zIndex: 8, resize: 'none', textAlign: 'justify', color: 'white'}} id="fourth_variable" name="field7" className="tex_box" placeholder="Insert your text" rows={1} cols={18} defaultValue={""} />
+                        {/* <select id="fifth_variable">
+                          <option value="none">None</option>
+                          <option value="yesterday">Yesterday</option>
+                          <option value="today">Today</option>
+                          <option value="goal">Daily Goal</option>
+                          <option value="other_user">Other User</option>
+                        </select>  */}
+                        <br /><br />
+                        <select id="mesage_type">
+                          <option value="fixa">Fixa</option>
+                          <option value="temporaria">Temporaria</option>
+                        </select> <br />
+                        <br />
+                        <input type="button" id="btAddText" defaultValue="Add Fixed Text" className="bt" />
+                        <input type="button" id="btRemoveText" defaultValue="Remove Fixed Text" className="bt" /><br /><br />
                     </div>
                   </div>
                 </div>
