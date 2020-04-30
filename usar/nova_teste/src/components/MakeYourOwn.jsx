b6fc32b153555a2295a8c7ea86382fcb9c8fbdd1
Tiago Baptista <61202028+tiagob99@users.noreply.github.com>
2020-04-30 15:51:52 +0100

    t

--- usar/nova_teste/src/components/MakeYourOwn.jsx
+++ usar/nova_teste/src/components/MakeYourOwn.jsx
@@ -9,109 +9,10 @@ import { bar_type } from './mikrosjs';
 import {GooglePicker, MyCustomButton} from "react-google-picker";
 import google from "react-google-picker";
 import gapi from 'gapi-client';
-import HTML2React from 'html2react';
+import './google';
 
-function Link (props) {
-  return <a {...props} style={{ textDecoration: 'none' }} />
-}
-const html = `<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta charset="utf-8" />
-    <title>Google Picker Example</title>
+    
 
-    <script type="text/javascript">
-
-    // The Browser API key obtained from the Google API Console.
-    // Replace with your own Browser API key, or your own key.
-    var developerKey = 'AIzaSyCXXXfI5Kx8-9lQvpFjglJOOiag8naMWjI';
-
-    // The Client ID obtained from the Google API Console. Replace with your own Client ID.
-    var clientId = "465234973180-endijv8herlk2sgucru2r0sis78t8auu.apps.googleusercontent.com"
-
-    // Replace with your own project number from console.developers.google.com.
-    // See "Project number" under "IAM & Admin" > "Settings"
-    var appId = "novo-269521";
-
-    // Scope to use to access user's Drive items.
-    var scope = ['https://www.googleapis.com/auth/drive.file'];
-
-    var pickerApiLoaded = false;
-    var oauthToken;
-
-    // Use the Google API Loader script to load the google.picker script.
-    function loadPicker() {
-      gapi.load('auth', {'callback': onAuthApiLoad});
-      gapi.load('picker', {'callback': onPickerApiLoad});
-    }
-
-    function onAuthApiLoad() {
-      window.gapi.auth.authorize(
-          {
-            'client_id': clientId,
-            'scope': scope,
-            'immediate': false
-          },
-          handleAuthResult);
-    }
-
-    function onPickerApiLoad() {
-      pickerApiLoaded = true;
-      createPicker();
-    }
-
-    function handleAuthResult(authResult) {
-      if (authResult && !authResult.error) {
-        oauthToken = authResult.access_token;
-        createPicker();
-      }
-    }
-
-    // Create and render a Picker object for searching images.
-    function createPicker() {
-      if (pickerApiLoaded && oauthToken) {
-        var view = new google.picker.View(google.picker.ViewId.DOCS);
-        view.setMimeTypes("image/png,image/jpeg,image/jpg");
-        var picker = new google.picker.PickerBuilder()
-            .enableFeature(google.picker.Feature.NAV_HIDDEN)
-            .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
-            .setAppId(appId)
-            .setOAuthToken(oauthToken)
-            .addView(view)
-            .addView(new google.picker.DocsUploadView())
-            .setDeveloperKey(developerKey)
-            .setCallback(pickerCallback)
-            .build();
-         picker.setVisible(true);
-      }
-    }
-
-    // A simple callback implementation.
-    function pickerCallback(data) {
-      if (data.action == google.picker.Action.PICKED) {
-        var fileId = data.docs[0].id;
-        alert('The user selected: ' + fileId);
-      }
-    }
-    </script>
-  </head>
-  <body>
-    <div id="result"></div>
-    <button onclick="showPickerDialog()">Show Picker Dialog</button>
-
-    <!-- The Google API Loader script. -->
-    <script type="text/javascript" src="https://apis.google.com/js/api.js"></script>
-    <script>
-    function showPickerDialog(){
-        loadPicker()
-    }
-    </script>
-  </body>
-</html>`
-const content = HTML2React(html, {
-  a: Link
-})      
-   
   
 const MakeYourOwn = () => {
   
@@ -188,7 +89,7 @@ const MakeYourOwn = () => {
     }
   }
   function showPickerDialog(){
-    $('teste').loadHTML('./teste.html');
+//    $('teste').loadHTML('./teste.html');
 }
     return (
       
@@ -275,7 +176,7 @@ const MakeYourOwn = () => {
       
                     <div id="result"></div>
                     {/* <div dangerouslySetInnerHTML={ {__html: htmlContent} } /> */}
-                   <button onClick={showPickerDialog}>Show Picker Dialog</button>
+                   {/* <button onClick={showPickerDialog}>Show Picker Dialog</button> */}
                    <div>
     {content}
   </div>
